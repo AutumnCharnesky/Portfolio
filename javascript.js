c9edd04f79477e321bfb2da6e0e5075b8a44274d
AutumnCharnesky <autumn.breeze.c18@gmail.com>
2017-04-24 16:08:45 -0400

    navbar and edits

--- javascript.js
+++ javascript.js
@@ -3,10 +3,12 @@ $(document).ready(function() {
   $(".topnav").find('a').hover(function() {
     $(this).animate({opacity: 0.25}, "slow", function() {
       $(this).css({"background-image": "url(images2/home.jpg)"})
-      .animate({opacity: 1})
-      $(this).css({"background-color": "rgba(159, 67, 9, 0.81)"});
+      .animate({opacity: 1});
     })
-  })
+    })
+      $(".topnav").find("a").on("click", function() {
+        $(this).css({"backgroundColor": "rgba(159, 67, 9, 0.81)"})
+    })
 
-
+    $(".topnav")
 });

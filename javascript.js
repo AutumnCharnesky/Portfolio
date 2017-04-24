03c9b7edb187857bfad5189a4f45e6f6c16db49b
AutumnCharnesky <autumn.breeze.c18@gmail.com>
2017-04-24 15:20:01 -0400

    navbar and fixes

--- javascript.js
+++ javascript.js
@@ -1,4 +1,12 @@
-function alertForm (form) {
-var TestVar = form.firstname.value+ " " +form.lastname.value;
-alert ("Thank you, " + TestVar);
-}
+$(document).ready(function() {
+
+  $(".topnav").find('a').hover(function() {
+    $(this).animate({opacity: 0.25}, "slow", function() {
+      $(this).css({"background-image": "url(images2/home.jpg)"})
+      .animate({opacity: 1})
+      $(this).css({"background-color": "rgba(159, 67, 9, 0.81)"});
+    })
+  })
+
+
+});
